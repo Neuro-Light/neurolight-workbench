cffa46010c36bb0260da013bf64fee32cfa71548
ajmastra <ajers03@gmail.com>
2026-03-16 18:14:30 -0700

    chore: tab order

--- src/ui/analysis_panel.py
+++ src/ui/analysis_panel.py
@@ -13,11 +13,12 @@ class AnalysisPanel(QTabWidget):
         self.neuron_detection_widget = NeuronDetectionWidget()
         self.neuron_trajectory_plot_widget = NeuronTrajectoryPlotWidget()
         self.rayleigh_plot_widget = RayLeighPlotWidget()
-        self._add_tab("Statistics")
-        self._add_tab("Graphs", self.neuron_trajectory_plot_widget)
-        self._add_tab("RayLeigh Plot", self.rayleigh_plot_widget)
-        self._add_tab("ROI Intensity", self.roi_plot_widget)
+        # Tab order: Detection, ROI Intensity, Graphs, Rayleigh Plot, Statistics
         self._add_tab("Detection", self.neuron_detection_widget)
+        self._add_tab("ROI Intensity", self.roi_plot_widget)
+        self._add_tab("Graphs", self.neuron_trajectory_plot_widget)
+        self._add_tab("Rayleigh Plot", self.rayleigh_plot_widget)
+        self._add_tab("Statistics")
 
     def _add_tab(self, title: str, widget: QWidget | None = None) -> None:
         if widget is None:
